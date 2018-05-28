c51285efeac9d6500215af57f28d1caee375d429
ingmachine <fredyjesus2013@gmail.com>
2018-05-27 23:49:27 -0500

    Comienzo

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Lenovo\PycharmProjects\CompiladorWeb\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
